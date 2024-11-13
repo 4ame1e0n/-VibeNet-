9698939ba9ea768b2b10310170049431a1224874
danial <danil.sagn@mail.ru>
2024-11-13 14:22:30 +0500

    main

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for, request, redirect, session, make_response
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+        app.run(debug=True)
+        socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+        #socketio.run(app, debug=True, use_reloader=False, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+

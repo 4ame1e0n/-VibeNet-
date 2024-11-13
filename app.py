13dae778dbbd07a321f074d07b40b884cc551ef8
danial <danil.sagn@mail.ru>
2024-11-13 19:06:39 +0500

    update inject logout

--- app.py
+++ app.py
@@ -80,7 +80,43 @@ def login():
 
 
 
+@app.route('/update_account/<int:id>', methods=['POST', 'GET'])
+def update_account(id):
+    user = User.query.get(id)
 
+    if request.method == 'POST':
+        user.username = request.form['username']
+        user.name = request.form['name']
+        user.password = generate_password_hash(request.form['password'], method='sha256')
+
+        try:
+            db.session.commit()
+            return redirect(url_for("user_page", user_id=user.id))
+        except:
+            return 'Error while updating account'
+
+    return render_template('update_account.html', user=user)
+
+
+# Context processor to make User class available globally
+@app.context_processor
+def inject_user():
+    if 'id' in session:
+        user_id = session['id']
+        user = User.query.get(user_id)
+        return dict(User=User, user=user)
+
+    return dict(User=None, user=None)
+
+
+
+
+@app.route("/logout")
+def logout():
+    session.pop('authenticated', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
 
